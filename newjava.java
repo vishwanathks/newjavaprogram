067dea79d623a0a35a3db999e819a40235818405
vishwanathks <vishwanathv30@gmail.com>
2019-03-27 11:43:44 +0530

    newline is added

--- newjava.java
+++ newjava.java
@@ -1,7 +1,9 @@
 public class newjava{
 	public static void main(String[] args){
 		for (int i=1;i<=10;i++){
-			System.out.println("Hello,World"+i);
+			System.out.println("Hello,World hellow vishwanath"+i);
 		}
 	}	
 {			
+
+
